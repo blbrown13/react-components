9069fb9528fb6bbc76436ecf0c180291adfe20a1
Hack Reactor Student <student@hackreactor.com>
2017-04-12 15:34:02 -0700

    Add app.js, completed react tutorial

--- app.js
+++ app.js
@@ -1,1 +1,60 @@
-// TODO
+// var Milk = () => (
+//   <li>Milk</li>
+// );
+//
+// var Beer = () => (
+//   <li>Beer</li>
+// );
+
+class GroceryListItems extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false,
+      hover: false
+    };
+  }
+
+
+  onListItemClick (event) {
+    this.setState({
+      done: !this.state.done
+    });
+    console.log('I got clicked');
+  }
+
+
+  // onMouseOver
+  onListItemHover (event) {
+    this.setState({
+      hover: !this.state.hover
+    });
+    console.log("I'm hovering");
+  }
+
+
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none',
+      fontWeight: this.state.hover ? 'bold' : 'lighter'
+    };
+
+    return (
+      <li style={style} onMouseOver={this.onListItemHover.bind(this)} onClick={this.onListItemClick.bind(this)}>{this.props.item}</li>
+    );
+  }
+
+};
+
+var GroceryList = (props) => (
+  <div>
+    <h2>Our Grocery List</h2>
+    <ul>
+      {props.items.map((item, index) =>
+        <GroceryListItems key={index} item={item}/>
+      )}
+    </ul>
+  </div>
+);
+
+ReactDOM.render(<GroceryList items={['Milk', 'Beer', 'Chocolate']} />, document.getElementById("app"));
